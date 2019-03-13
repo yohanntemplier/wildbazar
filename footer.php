d8df1289ab5fbc01d370ea8a8079b5ec61de07e3
yohanntemplier <templieryohann@gmail.com>
2019-03-13 10:52:34 +0100

    ajout footer php

---
+++ footer.php
@@ -0,0 +1,70 @@
+<?php
+?>
+<!-- Footer -->
+		<section class = "footer">
+			<footer class="container-fluid">
+				<div class="row footer">
+					<div class="col-sm">
+						<div class="row tableFooter">
+							<div class="col-6"><a class="lienfoot" href="index.html">Accueil</a></div>
+							<div class="col-6"><a class="lienfoot" href="PageTextiles.html">Textile</a></div>
+						</div>
+						<div class="row">
+							<div class="col-6"><a class="lienfoot" href="#">A propos de nous</a></div>
+							<div class="col-6"><a class="lienfoot" href="vaisselle.html">Vaisselle</a></div>
+						</div>
+						<div class="row">
+							<div class="col-6"><a class="lienfoot" href="#">Meilleures ventes</a></div>
+							<div class="col-6"><a class="lienfoot" href="page_animaux2.html">Animalerie</a></div>
+						</div>
+						<div class="row">
+							<div class="col-6"><a class="lienfoot" href="#">Contact</a></div>
+							<div class="col-6"><a class="lienfoot" href="papeterie.html">Papeterie</a></div>
+						</div>
+						<div class="row">
+							<div class="col-6"><a class="lienfoot" href="#">Mentions légales</a></div>
+							<div class="col-6"><a class="lienfoot" href="accessoire.html">Accessoires</a></div>
+						</div>
+						<br>
+					</div>
+					<div class="col-sm">
+						<div class = "wild">
+							<div class="col-8">
+							<p> <img src="https://imagizer.imageshack.com/img923/8125/xjvCju.png" class="logofooter" alt="logo"> <span class="titre"> W</span>ild<span class="titre">B</span>azar</p> </div>
+						</div>
+
+						<div class ="adress">
+							<div class="col-7 ">
+	      					<p>1 rue de la Paix<br>
+	      					75000 Paris<br>
+	      					01-23-45-67-89</p>
+	      					</div>
+      					</div>
+      				</div>
+
+    				<div class="col-12 col-lg-6 justify-content-center collumn-right">
+      					<h3 class="text-center" id="contactUs">Nous contacter</h3>
+      					<form>
+		      				<div class="form-group row">
+		      					<label for="inputEmail3" class="col-3 col-form-label">Email</label>
+		      					<div class="col-8">
+		      					<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+		      					</div>
+		      				</div>
+		      				<div class="form-group row">
+		      					<label for="inputPassword3" class="col-3 col-form-label">Message</label>
+		      					<div class="col-8">
+								<textarea class="form-control" id="inputPassword3" rows="3"></textarea>
+		      					</div>
+		      				</div>
+		      				<div class="form-group row">
+		      					<div class="col-12 text-center">
+		      					<button type="submit" class="btn">Envoyer</button>
+		      					</div>
+	      					</div>
+      					</form>
+      				</div>
+				</div>
+			</footer>
+		</section>
+<!--End Footer -->
