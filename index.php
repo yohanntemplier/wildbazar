84c21483a616fb93972de9b14833e5487ec070c7
Elodie Billon <elodie.billon1995@outlook.fr>
2019-03-13 13:20:53 +0100

    Ajout du footer.php + lien vers la page animalerie

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
 					</a>
 				</div>
 				<div class="col-sm-6 col-md-5 col-lg-4 c">
-					<a href="page_animaux.html" class="card c">
+					<a href="page_animaux3.php" class="card c">
 						<img src="https://images.pexels.com/photos/1378849/pexels-photo-1378849.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="">
 						<div class="card-img-overlay c">
 							<h5 class="card-title c">Animalerie</h5>
@@ -209,67 +209,9 @@
 			</div>
 		</div>
 	</section>
-	<footer class="container-fluid">
-		<div class="row footer">
-			<div class="col-sm">
-				<div class="row tableFooter">
-					<div class="col-5"><a class="lienfoot" href="#">Home</a></div>
-					<div class="col-5"><a class="lienfoot" href="#">Textile</a></div>
-				</div>
-				<div class="row">
-					<div class="col-5"><a class="lienfoot" href="#">About us</a></div>
-					<div class="col-5"><a class="lienfoot" href="#">Vaisselle</a></div>
-				</div>
-				<div class="row">
-					<div class="col-5"><a class="lienfoot" href="#">Top selling</a></div>
-					<div class="col-5"><a class="lienfoot" href="#">Animalerie</a></div>
-				</div>
-				<div class="row">
-					<div class="col-5"><a class="lienfoot" href="#">Contact</a></div>
-					<div class="col-5"><a class="lienfoot" href="#">Papeterie</a></div>
-				</div>
-				<div class="row">
-					<div class="col-5"><a class="lienfoot" href="#">Legal mentions</a></div>
-					<div class="col-5"><a class="lienfoot" href="#">Accessoires</a></div>
-				</div>
-			</div>
-			<div class="col-sm">
-				<div class="wild">
-					<div class="col-8">
-						<p><img src="https://imagizer.imageshack.com/img923/8125/xjvCju.png" class="logofooter" alt="logo"> 
-						<span class="titre"> W</span>ild<span class="titre">B</span>azar</p>
-					</div>
-				</div>
-				<div class="adress">
-					<div class="col-7 ">
-						<p>1 rue de la Paix<br> 75000 Paris<br> 01-23-45-67-89</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-12 col-lg-6 justify-content-center collumn-right">
-				<h3 class="text-center" id="contactUs">Contact us</h3>
-				<form>
-					<div class="form-group row">
-						<label for="inputEmail3" class="col-2 col-form-label">Email</label>
-						<div class="col-8">
-							<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-						</div>
-					</div>
-					<div class="form-group row">
-						<label for="inputPassword3" class="col-2 col-form-label">Message</label>
-						<div class="col-8">
-							<textarea class="form-control" id="inputPassword3" rows="3"></textarea>
-						</div>
-					</div>
-					<div class="form-group row">
-						<div class="col-12 text-center">
-							<button type="submit" class="btn">Submit</button>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</footer>
+    <?php
+        include 'footer.php';
+    ?>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
