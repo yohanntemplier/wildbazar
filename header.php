79954064f5b07511a97883ba3720dc907e23b5f0
yohanntemplier <templieryohann@gmail.com>
2019-03-15 10:49:39 +0100

    modif lien headerphp

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 				<div class="collapse navbar-collapse" id="navbarNavDropdown">
 						<ul class="navbar-nav">
 								<li class="nav-item active">
-										<a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
+										<a class="nav-link" href="index.php">Accueil<span class="sr-only">(current)</span></a>
 								</li>
 								<li class="nav-item">
 										<a class="nav-link" href="PageTextiles.php">Textiles</a>
