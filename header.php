4fc0a6c96c5fbff55243ec088145bd601c611365
Elodie Billon <elodie.billon1995@outlook.fr>
2019-03-22 10:59:21 +0100

    Jumbotron retiré du header.php

--- header.php
+++ header.php
@@ -29,9 +29,3 @@
             </ul>
     </div>
 </nav>
-<h2 class="title-banner col-12"><?php echo $categoryTitle ?></h2>
-<div class="row justify-content-center">
-    <p class="title-desc-banner col-12"><?php echo $categoryDescription ?></p>
-    <button type="button" class="btn btn1">+ ADD NEW</button>
-</div>
-<img src="<?php echo $categoryPicture ?>" class="img-cover" alt="Photo de couverture Wild Bazar Animalerie">
