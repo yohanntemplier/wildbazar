126d0d3d9f53ec49ef5c358746b92108a6ebd34b
Elodie Billon <elodie.billon1995@outlook.fr>
2019-03-25 10:06:43 +0100

    Ajout de la page succès pour le formulaire du petshop

---
+++ success_petform.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="form_petshop.css">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <title>Ajout d'un produit dans l'Animalerie</title>
+</head>
+<body>
+    <?php
+    include 'header.php'
+    ?>
+        <h2 class="title-banner col-12"><span class="s1">A</span>jouter un articl<span class="s1">e</span></h2>
+        <div class="row justify-content-center">
+            <button type="submit" class="btn btn1">+ ADD NEW</button>
+        </div>
+        <img src="images/cover-animalerie.jpeg" class="img-cover" alt="Photo de couverture Wild Bazar Animalerie">
+        <h2 class="success">Votre article a bien été ajouté !</h2>
+    <?php
+    include 'footer.php'
+    ?>
+</body>
+</html>
+
